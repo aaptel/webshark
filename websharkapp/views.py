fdf1e73d265c52bb313ecccf2fde80e92a3d9bb9
Aurelien Aptel <aaptel@suse.com>
2016-06-28 17:17:38 +0200

    draft of trace template
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>

--- websharkapp/views.py
+++ websharkapp/views.py
@@ -6,7 +6,8 @@ def index(request):
     return render(request, 'websharkapp/index.html', ctx)
 
 def show_trace(request, trace_id_str):
-    return HttpResponse("view trace %d page" % int(trace_id_str))
+    ctx = {'var': 42}
+    return render(request, 'websharkapp/trace.html', ctx)
 
 def new_trace(request):
     return HttpResponse("new trace page")
